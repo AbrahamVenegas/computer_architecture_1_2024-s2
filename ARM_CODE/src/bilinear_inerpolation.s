33475d7c6b236acc05f23e2a0ed643e3e9d69d02
Abner Arroyo <38987646+Abner2111@users.noreply.github.com>
2024-09-28 19:22:12 -0600

    [update] image chunk slicing

--- ARM_CODE/src/bilinear_inerpolation.s
+++ ARM_CODE/src/bilinear_inerpolation.s
@@ -5,6 +5,10 @@
 
 .section .data
     interp_matrix   .word   16
+    esquina_si      .word   0
+    esquina_sd      .word   0
+    esquina_ii      .word   0
+    esquina_id      .word   0
 .section .bss
 
     src_image:      .word    0
@@ -13,6 +17,48 @@
 
 _start: 
     ldr r0, image
+    mov r1, #0      @chunk counter
+    mov r2, #0      @y offset
+loop_image:
+    cmp r2, #100
+    bge exit
+    curr_2x2_row:
+    cmp r1, #100
+    ble continue_loop
+    mov r1, #0
+    add r2, #1
+    b loop_image
+    continue_loop:
+
+    @offset adding
+    mov r3, #0 
+    add r3, r3, r1 @add chunk counter
+    add r3, r3, r2 @add vertical offset
+
+    @superior izquierda
+    ldr r4, [r0, r3]
+    ldr r5, =esquina_si
+    str r4, [r5]
+
+    @superior derecha
+    add r3, r3, #1
+    ldr r4, [r0, r3]
+    str r5, =esquina_sd
+    str r4, [r5]
+
+    @inferior izquierda
+    add r3, r3, #99
+    ldr r4, [r0, r3]
+    str r5, =esquina_ii
+    str r4, [r5]
+
+    @inferior derecha
+    add r3, r3, #1
+    ldr r4, [r0, r3]
+    str r5, =esquina_id
+    str r4, [r5]
+
+    bl sides
 
 sides:
     
@@ -40,7 +86,8 @@ interpolation_for_sides:
     pop {r2-r9, lr}
     bx lr
 
-
+exit:
+    
 
 
     
